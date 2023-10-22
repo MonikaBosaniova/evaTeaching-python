d0475e5ccf0bd7819fa535c062871b1c94d7456f
MonikaBosaniova <48916222+MonikaBosaniova@users.noreply.github.com>
2023-10-22 09:38:50 +0000

    init sga hw

--- sga.py
+++ sga.py
@@ -3,34 +3,37 @@ import pprint
 
 import numpy as np
 
-POP_SIZE = 100
-IND_LEN = 25
+POP_SIZE = 200
+IND_LEN = 50
 CX_PROB = 0.8
 MUT_PROB = 0.05
 MUT_FLIP_PROB = 0.1
+
 
 # creates a single individual of lenght `lenght`
 def create_ind(length):
     return [random.randint(0, 1) for _ in range(length)]
 
+
 # creates a population of `size` individuals
 def create_population(size):
     return [create_ind(IND_LEN) for _ in range(size)]
 
+TOURNAMENT_COMPETITOR_COUNT = 2
 # tournament selection
-# def selection(pop, fits):
-#     selected = []
-#     for _ in range(len(pop)):
-#         i1, i2 = random.randrange(0, len(pop)), random.randrange(0, len(pop))
-#         if fits[i1] > fits[i2]:
-#             selected.append(pop[i1])
-#         else: 
-#             selected.append(pop[i2])
-#     return selected
+def tournament_selection(pop, fits):
+     selected = []
+     for _ in range(len(pop)):
+        parents = [random.randrange(0, len(pop)) for _ in range(TOURNAMENT_COMPETITOR_COUNT)]
+        selected.append(pop[max(parents, key=lambda ind:fits[ind])])
+     return selected
 
 # roulette wheel selection
-def selection(pop, fits):
+def roulette_selection(pop, fits):
     return random.choices(pop, fits, k=POP_SIZE)
+
+def selection(pop, fits):
+    return tournament_selection(pop, fits)
 
 # one point crossover
 def cross(p1, p2):
@@ -38,6 +41,7 @@ def cross(p1, p2):
     o1 = p1[:point] + p2[point:]
     o2 = p2[:point] + p1[point:]
     return o1, o2
+
 
 # applies crossover to all individuals
 def crossover(pop):
@@ -50,24 +54,29 @@ def crossover(pop):
         off.append(o2)
     return off
 
+
 # bit-flip mutation
 def mutate(p):
     if random.random() < MUT_PROB:
         return [1 - i if random.random() < MUT_FLIP_PROB else i for i in p]
     return p[:]
-    
+
+
 # applies mutation to the whole population
 def mutation(pop):
     return list(map(mutate, pop))
+
 
 # applies crossover and mutation
 def operators(pop):
     pop1 = crossover(pop)
     return mutation(pop1)
 
+
 # evaluates the fitness of the individual
 def fitness(ind):
-    return sum(ind)
+    return max(((ind[0::2]).count(0) + (ind[1::2]).count(1), ((ind[1::2]).count(1) + (ind[0::2]).count(0))))
+
 
 # implements the whole EA
 def evolutionary_algorithm(fitness):
@@ -75,14 +84,15 @@ def evolutionary_algorithm(fitness):
     log = []
     for G in range(100):
         fits = list(map(fitness, pop))
-        log.append((G, max(fits), sum(fits)/100, G*POP_SIZE))
-        #print(G, sum(fits), max(fits)) # prints fitness to console
+        log.append((G, max(fits), sum(fits) / 100, G * POP_SIZE))
+        # print(G, sum(fits), max(fits)) # prints fitness to console
         mating_pool = selection(pop, fits)
         offspring = operators(mating_pool)
-        #pop = offspring[:-1]+[max(pop, key=fitness)] #SGA + elitism
-        pop = offspring[:] #SGA
+        pop = offspring[:-1]+[max(pop, key=fitness)] #SGA + elitism
+        #pop = offspring[:]  # SGA
 
     return pop, log
+
 
 # i1, i2 = create_ind(10), create_ind(10)
 # print((i1, i2))
@@ -90,29 +100,37 @@ def evolutionary_algorithm(fitness):
 # print(mutate(i1))
 
 # run the EA 10 times and aggregate the logs, show the last gen in last run
-logs = []
-for i in range(10):
-    random.seed(i)
-    pop,log = evolutionary_algorithm(fitness)
-    logs.append(log)
-fits = list(map(fitness, pop))
-# pprint.pprint(list(zip(fits, pop)))
-# print(sum(fits), max(fits))
-# pprint.pprint(log)
+for j in range(10):
+    print("run: " + str(j))
+    CX_PROB = j/10
+    TOURNAMENT_COMPETITOR_COUNT += 1
+    #MUT_PROB = j/10
+    #MUT_FLIP_PROB = 0.2
+    logs = []
+    for i in range(10):
+        random.seed(i)
+        pop, log = evolutionary_algorithm(fitness)
+        logs.append(log)
+    fits = list(map(fitness, pop))
+    # pprint.pprint(list(zip(fits, pop)))
+    # print(sum(fits), max(fits))
+    # pprint.pprint(log)
 
-# extract fitness evaluations and best fitnesses from logs
-evals = []
-best_fit = []
-for log in logs:
-    evals.append([l[3] for l in log])
-    best_fit.append([l[1] for l in log])
+    # extract fitness evaluations and best fitnesses from logs
+    evals = []
+    best_fit = []
+    for log in logs:
+        evals.append([l[3] for l in log])
+        best_fit.append([l[1] for l in log])
 
-evals = np.array(evals)
-best_fit = np.array(best_fit)
+    evals = np.array(evals)
+    best_fit = np.array(best_fit)
 
-# plot the converegence graph and quartiles
-import matplotlib.pyplot as plt
-plt.plot(evals[0,:], np.median(best_fit, axis=0))
-plt.fill_between(evals[0,:], np.percentile(best_fit, q=25, axis=0),
-                             np.percentile(best_fit, q=75, axis=0), alpha = 0.2)
-plt.show()
+    # plot the converegence graph and quartiles
+    import matplotlib.pyplot as plt
+
+    plt.plot(evals[0, :], np.median(best_fit, axis=0), label="CX_PROB: " + str(CX_PROB) + " | MUT_PROB: " + str(MUT_PROB))
+    plt.fill_between(evals[0, :], np.percentile(best_fit, q=25, axis=0),
+                     np.percentile(best_fit, q=75, axis=0), alpha=0.2)
+plt.legend()
+plt.savefig('sga.png', bbox_inches='tight', dpi=600)
